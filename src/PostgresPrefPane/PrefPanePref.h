1a3d5e93f54480505edc360248906f0c2488271b
david.thorpe <david.thorpe@9500d0c1-364d-0410-ab8a-c31d527c05bf>
2009-02-11 20:15:07 +0000

    updated
    git-svn-id: https://postgres-kit.googlecode.com/svn/trunk@16 9500d0c1-364d-0410-ab8a-c31d527c05bf

---
+++ src/PostgresPrefPane/PrefPanePref.h
@@ -0,0 +1,19 @@
+//
+//  PrefPanePref.h
+//  PrefPane
+//
+//  Created by David Thorpe on 11/02/2009.
+//  Copyright (c) 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <PreferencePanes/PreferencePanes.h>
+
+
+@interface PrefPanePref : NSPreferencePane 
+{
+
+}
+
+- (void) mainViewDidLoad;
+
+@end
---
+++ src/PostgresPrefPane/PrefPanePref.m
@@ -0,0 +1,18 @@
+//
+//  PrefPanePref.m
+//  PrefPane
+//
+//  Created by David Thorpe on 11/02/2009.
+//  Copyright (c) 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import "PrefPanePref.h"
+
+
+@implementation PrefPanePref
+
+- (void) mainViewDidLoad
+{
+}
+
+@end
