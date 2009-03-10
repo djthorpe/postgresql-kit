8ae6e66a037a99c984fc26fecaeebb120df72909
david.thorpe <david.thorpe@9500d0c1-364d-0410-ab8a-c31d527c05bf>
2009-03-10 21:39:29 +0000

    git-svn-id: https://postgres-kit.googlecode.com/svn/trunk@117 9500d0c1-364d-0410-ab8a-c31d527c05bf

--- src/UnitTests/ClientConnection.h
+++ src/UnitTests/ClientConnection.h
@@ -1,8 +1,11 @@
 
 #import <SenTestingKit/SenTestingKit.h>
+#import <PostgresServerKit/PostgresServerKit.h>
 
 @interface ClientConnection : SenTestCase {
-
+	FLXServer* server;
 }
+
+@property (retain) FLXServer* server;
 
 @end
--- src/UnitTests/ClientConnection.m
+++ src/UnitTests/ClientConnection.m
@@ -3,11 +3,38 @@
 #import <PostgresClientKit/PostgresClientKit.h>
 
 @implementation ClientConnection
+@synthesize server;
+
+////////////////////////////////////////////////////////////////////////////////
+
+-(void)setUp {
+    // Run before ALL test methods
+	FLXServer* theServer = [FLXServer sharedServer];
+	[self setServer:theServer];
+}	
+
++(void)tearDown {
+    // Run after ALL test method
+	NSLog(@"TOTAL tearDown");
+}	
+
+////////////////////////////////////////////////////////////////////////////////
 
 -(void)testCreateConnectionObject {
 	FLXPostgresConnection* theConnection = [[FLXPostgresConnection alloc] init];
 
-	// do stuff
+	STAssertNotNil([self server],@"Shared server should not be nil");
+
+	
+	
+	[theConnection release];
+}
+
+-(void)testCreateConnectionObject2 {
+	FLXPostgresConnection* theConnection = [[FLXPostgresConnection alloc] init];
+	
+	
+	
 	
 	[theConnection release];
 }
