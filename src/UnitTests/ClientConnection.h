62ef405855487bb1e83c4ea3e10218199181f456
david.thorpe <david.thorpe@9500d0c1-364d-0410-ab8a-c31d527c05bf>
2009-03-13 09:40:33 +0000

    git-svn-id: https://postgres-kit.googlecode.com/svn/trunk@118 9500d0c1-364d-0410-ab8a-c31d527c05bf

--- src/UnitTests/ClientConnection.h
+++ src/UnitTests/ClientConnection.h
@@ -1,11 +1,14 @@
 
 #import <SenTestingKit/SenTestingKit.h>
 #import <PostgresServerKit/PostgresServerKit.h>
+#import <PostgresClientKit/PostgresClientKit.h>
 
 @interface ClientConnection : SenTestCase {
 	FLXServer* server;
+	FLXPostgresConnection* client;
 }
 
 @property (retain) FLXServer* server;
+@property (retain) FLXPostgresConnection* client;
 
 @end
--- src/UnitTests/ClientConnection.m
+++ src/UnitTests/ClientConnection.m
@@ -1,42 +1,140 @@
 
 #import "ClientConnection.h"
-#import <PostgresClientKit/PostgresClientKit.h>
 
 @implementation ClientConnection
 @synthesize server;
+@synthesize client;
 
 ////////////////////////////////////////////////////////////////////////////////
 
--(void)setUp {
-    // Run before ALL test methods
-	FLXServer* theServer = [FLXServer sharedServer];
-	[self setServer:theServer];
-}	
-
-+(void)tearDown {
-    // Run after ALL test method
-	NSLog(@"TOTAL tearDown");
-}	
-
-////////////////////////////////////////////////////////////////////////////////
-
--(void)testCreateConnectionObject {
-	FLXPostgresConnection* theConnection = [[FLXPostgresConnection alloc] init];
-
-	STAssertNotNil([self server],@"Shared server should not be nil");
-
++(void)stopServer:(FLXServer* )theServer {	
+	// stop the server
+	BOOL isSuccess = YES;
+	isSuccess = [theServer stop];
+	if(isSuccess==NO) NSLog(@"Unable to initiate server stop");
+//	NSParameterAssert(isSuccess);
 	
+	NSUInteger theClock = 0;
+	while([theServer state] != FLXServerStateStopped && theClock < 5) {
+		NSLog(@"server state = %@",[theServer stateAsString]);
+		[NSThread sleepUntilDate:[NSDate dateWithTimeIntervalSinceNow:1.0]];
+		theClock++;
+	}
 	
-	[theConnection release];
+	if([theServer state] != FLXServerStateStopped) {
+		NSLog(@"Unable to stop server: %@",[theServer stateAsString]);
+		//NSParameterAssert(NO);
+	}	
 }
 
--(void)testCreateConnectionObject2 {
-	FLXPostgresConnection* theConnection = [[FLXPostgresConnection alloc] init];
++(void)startServer:(FLXServer* )theServer {
+	NSLog(@"starting server....");
 	
+	// Path should be home directory
+	NSString* theTempDirectory = NSTemporaryDirectory();
+	NSString* theDataDirectory = [theTempDirectory stringByAppendingString:@"postgres-kit-test"];
+
+	NSError* theError = nil;
+	BOOL isSuccess = YES;
+
+	NSLog(@"data directory = %@",theDataDirectory);
 	
+	// remove data directory
+	if([[NSFileManager defaultManager] fileExistsAtPath:theDataDirectory]==YES) {	
+		NSLog(@"stopping server....");
+		[self stopServer:theServer];		
+		NSLog(@"removing path %@",theDataDirectory);
+		isSuccess = [[NSFileManager defaultManager] removeItemAtPath:theDataDirectory error:&theError];
+		if(theError) NSLog(@"%@: %@",theDataDirectory,[theError localizedDescription]);
+	}
+	NSParameterAssert(isSuccess);
+
+	// create data directory
+	isSuccess = [[NSFileManager defaultManager] createDirectoryAtPath:theDataDirectory attributes:nil];
+	if(isSuccess==NO) NSLog(@"%@: Unable to create directory",theDataDirectory);
+	NSParameterAssert(isSuccess);
+
+	NSLog(@"created directory = %@",theDataDirectory);
+		
+	// start the server
+	isSuccess = [theServer startWithDataPath:theDataDirectory];
+	if(isSuccess==NO) NSLog(@"Unable to initiate startWithDataPath");
+	NSParameterAssert(isSuccess);
+
+	NSUInteger theClock = 0;
+	while([theServer state] != FLXServerStateStarted && theClock < 60) {
+		
+		NSLog(@"server state = %@",[theServer stateAsString]);
+		
+		if([theServer state]==FLXServerStateStopped || [theServer state]==FLXServerStateStartingError) {
+			NSLog(@"Unable to start server: %@",[theServer stateAsString]);
+			NSParameterAssert(NO);
+		}
+		[NSThread sleepUntilDate:[NSDate dateWithTimeIntervalSinceNow:1.0]];
+		theClock++;
+	}
+
+	if([theServer isRunning]==NO) {
+		NSLog(@"Server could not be started");
+		NSParameterAssert(NO);
+	}			
+}
+
++(void)tearDown {
+	NSLog(@"doing teardown");
 	
+	[self stopServer:[FLXServer sharedServer]];
 	
-	[theConnection release];
+	// remove the data directory
+	NSString* theDataDirectory = [[FLXServer sharedServer] dataPath];
+	BOOL isSuccess = YES;
+	NSError* theError = nil;	
+	if([[NSFileManager defaultManager] fileExistsAtPath:theDataDirectory]==YES) {
+		isSuccess = [[NSFileManager defaultManager] removeItemAtPath:theDataDirectory error:&theError];
+		if(theError) NSLog(@"%@: %@",theDataDirectory,[theError localizedDescription]);
+	}
+	NSParameterAssert(isSuccess);
+}	
+
+-(void)setUp {
+	
+	//////// server
+	[self setServer:[FLXServer sharedServer]];		
+	if([[self server] isRunning]==NO) {
+		[[self class] startServer:[self server]];
+	}
+	
+	//////// client
+	FLXPostgresConnection* theConnection = [[[FLXPostgresConnection alloc] init] autorelease];
+	@try {
+		[theConnection setDatabase:@"postgres"];
+		[theConnection setUser:[FLXServer superUsername]];
+		[theConnection connect];
+	} @catch(NSException* theException) {
+		NSLog(@"%@",theException);
+	}	
+	STAssertTrue([theConnection connected],@"Connection not made");	
+	[self setClient:theConnection];
+}	
+
+-(void)tearDown {
+	@try {
+		[[self client] disconnect];
+	} @catch(NSException* theException) {
+		NSLog(@"%@",theException);		
+	}
+	STAssertTrue([[self client] connected]==NO,@"Connection still active");	
+	[self setClient:nil];
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+-(void)testPing {
+	FLXPostgresResult* theResult = [[self client] execute:@"SELECT 1"];
+	STAssertNotNil(theResult,@"No result");	
+	STAssertTrue([theResult isDataReturned],@"No data returned");	
+	STAssertEquals([theResult affectedRows],((NSUInteger)1),@"Requires one row returned");
+	NSLog(@"data returned = %@",[theResult fetchRowAsArray]);
 }
 
 @end
