ddebc78f8f6421bfe695fc9b59b5e033d0e9c205
david.thorpe <david.thorpe@9500d0c1-364d-0410-ab8a-c31d527c05bf>
2009-05-05 08:05:46 +0000

    git-svn-id: https://postgres-kit.googlecode.com/svn/trunk@230 9500d0c1-364d-0410-ab8a-c31d527c05bf

--- src/Example/TypeTests/Application.m
+++ src/Example/TypeTests/Application.m
@@ -27,18 +27,31 @@
 
 ////////////////////////////////////////////////////////////////////////////
 
+-(NSObject* )valueForType:(NSString* )theType row:(NSUInteger)row {
+	if([theType isEqual:@"int2"]) {
+		return [NSNumber numberWithShort:row];
+	} else {
+		return [NSNull null];
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////
+
 -(void)doWork {
 	NSString* theSchema = @"public";
 	NSString* theTable = @"test";
 	NSUInteger numberOfRows = 1000;
 	NSArray* thePostgresTypes = [NSArray arrayWithObjects:@"text",@"int2",@"int4",@"int8",@"float4",@"float8",nil];
-	NSArray* theObjectTypes = [NSArray arrayWithObjects:@"NSString",@"NSNumber",@"NSNumber",@"NSNumber",@"NSNumber",@"NSNumber",nil];
+	NSArray* theObjectClasses = [NSArray arrayWithObjects:@"NSString",@"NSNumber",@"NSNumber",@"NSNumber",@"NSNumber",@"NSNumber",nil];
 
 	// connect to database
 	[[self connection] connect];
 	
 	// iterate through the types
-	for(NSString* thePostgresType in thePostgresTypes) {
+	for(NSUInteger i = 0; i < [thePostgresTypes count]; i++) {
+		NSString* thePostgresType = [thePostgresTypes objectAtIndex:i];
+		Class theObjectClass = NSClassFromString([theObjectClasses objectAtIndex:i]);
+		NSParameterAssert(theObjectClass);
 		
 		// delete existing table		
 		if([[[self connection] tablesInSchema:theSchema] containsObject:theTable]) {
@@ -46,12 +59,28 @@
 		}
 		
 		// create a new table
-		[[self connection] executeWithFormat:@"CREATE TABLE %@.%@ (a %@)",theSchema,theTable,thePostgresType];	
+		[[self connection] executeWithFormat:@"CREATE TABLE %@.%@ (id SERIAL PRIMARY KEY,value %@)",theSchema,theTable,thePostgresType];	
 
+		// generate data
+		NSMutableArray* theData = [[NSMutableArray alloc] initWithCapacity:numberOfRows];
+		for(NSUInteger row = 0; row < numberOfRows; row++) {
+			NSObject* theValue = [self valueForType:thePostgresType row:row];
+			NSParameterAssert([theValue isKindOfClass:[NSNull class]] || [theValue isKindOfClass:theObjectClass]);
+			[theData addObject:theValue];			
+		}
+
+		// prepare statement
+		FLXPostgresStatement* theInsert = [[self connection] prepareWithFormat:@"INSERT INTO %@.%@ (value) VALUES ($1)",theSchema,theTable];
+		
 		// insert data into the table
 		for(NSUInteger row = 0; row < numberOfRows; row++) {
-			
+			[[self connection] executePrepared:theInsert value:[theData objectAtIndex:row]];
 		}
+		
+		// read data back from table and compare to original data
+		
+		// release data
+		[theData release];							  
 	}
 	
 	// disconnect from database
@@ -65,7 +94,11 @@
 }
 
 -(void)connection:(FLXPostgresConnection* )theConnection willExecute:(NSObject* )theQuery values:(NSArray* )theValues {
-	NSLog(@"Query: %@",theQuery);
+	if([theQuery isKindOfClass:[FLXPostgresStatement class]]) {
+		NSLog(@"Query: %@",[(FLXPostgresStatement* )theQuery statement]);
+	} else {
+		NSLog(@"Query: %@",theQuery);		
+	}
 }
 
 @end
