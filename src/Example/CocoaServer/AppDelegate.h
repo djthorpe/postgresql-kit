6603a356da8accbce370699dd631c28a5637767c
david.thorpe <david.thorpe@9500d0c1-364d-0410-ab8a-c31d527c05bf>
2009-11-30 21:18:02 +0000

    git-svn-id: https://postgres-kit.googlecode.com/svn/trunk@291 9500d0c1-364d-0410-ab8a-c31d527c05bf

--- src/Example/CocoaServer/AppDelegate.h
+++ src/Example/CocoaServer/AppDelegate.h
@@ -4,13 +4,24 @@
 
 @interface AppDelegate : NSObject <NSApplicationDelegate> {
     NSWindow* window;
-	NSMutableString* textField;
+	NSString* textField;
+	NSString* serverStatusField;
+	NSString* backupStatusField;
+	NSMutableString* textLog;
 }
 
 @property (assign) IBOutlet NSWindow* window;
-@property (retain) NSMutableString* textField;
+@property (retain) NSString* textField;
+@property (retain) NSMutableString* textLog;
 @property (readonly) FLXPostgresServer* server;
 @property (readonly) NSString* dataPath;
 @property (retain) NSTimer* timer;
+@property (retain) NSString* serverStatusField;
+@property (retain) NSString* backupStatusField;
+
+-(IBAction)doServerStart:(id)sender;
+-(IBAction)doServerStop:(id)sender;
+-(IBAction)doServerBackup:(id)sender;
+-(IBAction)doServerAccess:(id)sender;	
 
 @end
--- src/Example/CocoaServer/AppDelegate.m
+++ src/Example/CocoaServer/AppDelegate.m
@@ -4,9 +4,13 @@
 @implementation AppDelegate
 
 @synthesize window;
+@synthesize textField;
+@synthesize textLog;
 @synthesize timer;
 @dynamic server;
 @dynamic dataPath;
+@synthesize serverStatusField;
+@synthesize backupStatusField;
 
 ////////////////////////////////////////////////////////////////////////////////
 // properties
@@ -22,21 +26,33 @@
 	return [[theApplicationSupportDirectory objectAtIndex:0] stringByAppendingPathComponent:theIdent];
 }
 
+
+-(void)addLogMessage:(NSString* )theMessage {
+	[[self textLog] appendFormat:@"%@\n",theMessage];
+    [self willChangeValueForKey:@"textField"];
+	[self setTextField:[[self textLog] copy]];
+    [self didChangeValueForKey:@"textField"];
+}
+
+
 ////////////////////////////////////////////////////////////////////////////////
 // FLXPostgresServerDelegate
 
 -(void)serverMessage:(NSString* )theMessage {	
-	NSLog(@"%@",theMessage);
+	[self addLogMessage:theMessage];
 }
 
--(void)serverStateDidChange:(NSString* )theMessage {
-	NSLog(@"STATE %@",theMessage);	
+-(void)serverStateDidChange:(NSString* )theMessage {	
+	[self setServerStatusField:[[self server] stateAsString]];
+	[self setBackupStatusField:[[self server] backupStateAsString]];
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
 -(void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-
+	// set text field
+	[self setTextLog:[[NSMutableString alloc] init]];
+	
 	// create a timer
 	NSTimer* theTimer = [NSTimer scheduledTimerWithTimeInterval:0.1 target:self selector:@selector(timerFired:) userInfo:nil repeats:YES];
 	[self setTimer:theTimer];
@@ -56,6 +72,25 @@
 	// invalidate the timer - remove from run loop
 	[[self timer] invalidate];
 }
+
+////////////////////////////////////////////////////////////////////////////////
+
+-(IBAction)doServerStart:(id)sender {
+	[[self server] startWithDataPath:[self dataPath]];
+}
+
+-(IBAction)doServerStop:(id)sender {
+	[[self server] stop];
+}
+
+-(IBAction)doServerBackup:(id)sender {
+	
+}
+
+-(IBAction)doServerAccess:(id)sender {
+	
+}
+
 
 ////////////////////////////////////////////////////////////////////////////////
 
