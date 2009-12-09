2ec3c84e5e980a8a2555a6f609daa897e2967240
david.thorpe <david.thorpe@9500d0c1-364d-0410-ab8a-c31d527c05bf>
2009-12-09 20:53:41 +0000

    git-svn-id: https://postgres-kit.googlecode.com/svn/trunk@312 9500d0c1-364d-0410-ab8a-c31d527c05bf

--- src/Example/CocoaServer/IdentityUserArrayController.m
+++ src/Example/CocoaServer/IdentityUserArrayController.m
@@ -4,10 +4,17 @@
 
 @implementation IdentityUserArrayController
 @synthesize ibTableView;
+@synthesize ibGroupsArrayController;
 
 // addObject overridden to allow table cell to be edited on insert
 -(void)addObject:(id) newObject {
 	[super addObject:newObject];
+	
+	// add in the group name
+	NSString* theGroup = [[self ibGroupsArrayController] selectedGroup];
+	[newObject setGroup:theGroup];
+
+	// start editing the row
 	NSInteger row = [[self arrangedObjects] indexOfObjectIdenticalTo:newObject];
 	[[self ibTableView] editColumn:0 row:row withEvent:nil select:YES];
 }
