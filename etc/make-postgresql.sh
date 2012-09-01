ad81df853fa3992c46b41234aa2beddda65d6321
djt@mutablelogic.com <djt@mutablelogic.com@9500d0c1-364d-0410-ab8a-c31d527c05bf>
2012-09-01 16:01:02 +0000

    Changes
    git-svn-id: https://postgres-kit.googlecode.com/svn/trunk@319 9500d0c1-364d-0410-ab8a-c31d527c05bf

---
+++ etc/make-postgresql.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+# Make postgresql from source in the tar subdirectory
+# Syntax:
+#   make-postgresql.sh ${INPUT_TAR} ${OUTPUT_DIR}
+
+CLEAN=0
+CURRENT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+UNARCHIVE=${DERIVED_SOURCES_DIR}
+TARZ=${1}
+BUILD=${2}
+VERSION=`basename ${TARZ} | sed 's/\.tar\.gz//'`
+
+# Check for the TAR file to make sure it exists
+if [ "${TARZ}XX" == "XX" ] || [ ! -e ${TARZ} ]
+then
+	echo "Syntax error: make-postgresql.sh {INPUT_TAR_GZ} {OUTPUT_FOLDER}"
+	exit 1
+fi
+
+# Check for the BUILD directory
+if [ "${BUILD}XX" == "XX" ] || [ ! -d ${BUILD} ]
+then
+    echo "Syntax error: make-postgresql.sh {INPUT_TAR_GZ} {OUTPUT_FOLDER}"
+    exit 1
+fi
+
+# Check for the UNARCHIVE  directories, use TMP if necessary
+if [ "${UNARCHIVE}XX" == "XX" ]
+then
+	UNARCHIVE=${TMPDIR}/${VERSION}/src
+fi
+if [ ! -d ${UNARCHIVE} ]
+then
+	echo "mkdir ${UNARCHIVE}"
+	mkdir -pv ${UNARCHIVE}
+fi
+
+##############################################################
+
+PREFIX=${BUILD}/${VERSION}
+
+if [ -e ${PREFIX} ] && [ ${CLEAN} == 0 ]
+then
+  echo "Assuming already exists: ${PREFIX}"
+  exit 0
+fi
+
+echo "Unarchiving sources to ${UNARCHIVE}"
+echo "Built postgres with be installed at ${PREFIX}"
+
+# remove existing build directory, unarchive
+rm -fr "${UNARCHIVE}"
+mkdir "${UNARCHIVE}"
+tar -C ${UNARCHIVE} -zxvf ${TARZ}
+
+# configure - for 10.6, we only support 64-bit architecture
+cd "${UNARCHIVE}/${VERSION}"
+export CFLAGS="-arch x86_64"
+export LDFLAGS="-arch x86_64"
+./configure --prefix="${PREFIX}" --enable-thread-safety --without-readline --with-bonjour --with-openssl --with-libxml --with-libxslt --disable-rpath
+
+# make and install
+make
+make install
+
+# make symbolic link
+rm -f ${BUILD}/postgresql-current
+ln -s ${PREFIX} ${BUILD}/postgresql-current
+exit 0
+
