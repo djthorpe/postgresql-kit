145dc90dd2a982c9d869e1e122e2f77981bbdc74
djt@mutablelogic.com <djt@mutablelogic.com@9500d0c1-364d-0410-ab8a-c31d527c05bf>
2012-09-02 13:26:31 +0000

    updated
    git-svn-id: https://postgres-kit.googlecode.com/svn/trunk@337 9500d0c1-364d-0410-ab8a-c31d527c05bf

---
+++ src/Example/PGServer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PGServer
+//
+//  Created by David Thorpe on 02/09/2012.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ src/Example/PGServer/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  PGServer
+//
+//  Created by David Thorpe on 02/09/2012.
+//
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
