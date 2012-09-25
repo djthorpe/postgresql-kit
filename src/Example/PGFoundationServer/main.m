62d7643df284965a4849851710b3e2c9f2bac3e0
djt@mutablelogic.com <djt@mutablelogic.com@9500d0c1-364d-0410-ab8a-c31d527c05bf>
2012-09-25 20:24:32 +0000

    Updates
    git-svn-id: https://postgres-kit.googlecode.com/svn/trunk@356 9500d0c1-364d-0410-ab8a-c31d527c05bf

--- src/Example/PGFoundationServer/main.m
+++ src/Example/PGFoundationServer/main.m
@@ -82,6 +82,8 @@
 		isRunning = [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode beforeDate:theNextDate]; 
 	} while(isRunning==YES && [self signal] >= 0); 
 
+	NSLog(@"Ended");
+	
 	return [self returnValue];
 }
 
@@ -97,6 +99,7 @@
 	if([[self server] state]==PGServerStateUnknown) {
 		BOOL isStarting = [[self server] startWithDataPath:[self dataPath]];
 		if(isStarting==NO) {
+			NSLog(@"Unable to start server, stopping");
 			[self setReturnValue:-1];
 			[[self server] stop];
 		}
