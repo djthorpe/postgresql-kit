c71bba77e209f9a2d0cfebe2fc91e64231126b97
djt@mutablelogic.com <djt@mutablelogic.com@9500d0c1-364d-0410-ab8a-c31d527c05bf>
2012-11-04 12:04:19 +0000

    commit
    git-svn-id: https://postgres-kit.googlecode.com/svn/trunk@403 9500d0c1-364d-0410-ab8a-c31d527c05bf

---
+++ Tokenizer/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Tokenizer
+//
+//  Created by David Thorpe on 04/11/2012.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
