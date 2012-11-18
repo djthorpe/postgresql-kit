e9c1db99dbef9033f1a68e809666f21be61b3902
djt@mutablelogic.com <djt@mutablelogic.com@9500d0c1-364d-0410-ab8a-c31d527c05bf>
2012-11-18 18:42:07 +0000

    Updated Tokenizer
    git-svn-id: https://postgres-kit.googlecode.com/svn/trunk@488 9500d0c1-364d-0410-ab8a-c31d527c05bf

--- src/Example/Tokenizer/PGServerConfiguration.h
+++ src/Example/Tokenizer/PGServerConfiguration.h
@@ -24,5 +24,14 @@
 -(BOOL)load;
 
 -(NSObject* )valueForKey:(NSString* )key;
+-(NSString* )suffixForKey:(NSString* )key;
+-(BOOL)enabledForKey:(NSString* )key;
+-(NSString* )commentForKey:(NSString* )key;
+
+-(void)setValue:(NSObject* )value enabled:(BOOL)enabled forKey:(NSString* )key error:(NSError** )error;
+-(void)setString:(NSString* )value enabled:(BOOL)enabled forKey:(NSString* )key error:(NSError** )error;
+-(void)setInteger:(NSInteger)value enabled:(BOOL)enabled forKey:(NSString* )key error:(NSError** )error;
+-(void)setDouble:(double)value enabled:(BOOL)enabled forKey:(NSString* )key error:(NSError** )error;
+-(void)setBool:(BOOL)value enabled:(BOOL)enabled forKey:(NSString* )key error:(NSError** )error;
 
 @end
