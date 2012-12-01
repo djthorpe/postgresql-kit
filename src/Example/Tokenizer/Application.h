d371692f223b5a43ade0bd367803435aa9a3e642
djthorpe <djt@mutablelogic.com>
2012-12-01 12:06:31 +0000

    changes

---
+++ src/Example/Tokenizer/Application.h
@@ -0,0 +1,8 @@
+
+#import <Cocoa/Cocoa.h>
+
+@interface Application : NSObject {
+	IBOutlet NSWindow* _mainWindow;
+}
+
+@end
---
+++ src/Example/Tokenizer/Application.m
@@ -0,0 +1,15 @@
+
+#import "Application.h"
+
+@implementation Application
+
+-(void)awakeFromNib {
+	NSLog(@"awake from nib");
+}
+
+-(IBAction)ibFileOpen:(id)sender {
+	// open the sheet which allows you to select a pg_hba.conf file
+
+}
+
+@end
