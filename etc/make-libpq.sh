59b7b4eb37823532e2f116ffadca34e05bae8196
djthorpe <djt@mutablelogic.com>
2013-07-25 22:17:50 +0100

    commit

--- etc/make-libpq.sh
+++ etc/make-libpq.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-# Make openssl from source, and create a symbolic link
+# Make libpq from source, and create a symbolic link
 # to the compiled package.
 #
 # Syntax:
-#   make-openssl.sh (input_tar) (output_directory) (flags)
+#   make-libpq.sh (input_tar) (output_directory) (flags)
 #
 # Flags:
 #   --clean will always rebuild from clean sources
 #   --platform=<platform> will built for one of these
 #      architectures:
 #         mac_x86_64 ios_armv7 ios_armv7s ios_simulator
+#   --openssl=<openssl> will use external version of
+#      openssl, previously built
 
 ##############################################################
 # Process command line arguments
@@ -21,17 +23,21 @@ TARZ=${1}
 BUILD=${2}
 CLEAN=0
 PLATFORM=mac_x86_64
+OPENSSL=
 
 for ARG in "$@"
 do
-    case ${ARG} in
-	  --clean )
-	    CLEAN=1
-		;;
-	  --platform=* )
-	    PLATFORM=`echo ${ARG} | sed 's/[-a-zA-Z0-9]*=//'`
-        ;;
-	esac
+  case ${ARG} in
+    --clean )
+      CLEAN=1
+      ;;
+    --platform=* )
+      PLATFORM=`echo ${ARG} | sed 's/[-a-zA-Z0-9]*=//'`
+      ;;
+    --openssl=* )
+      OPENSSL=`echo ${ARG} | sed 's/[-a-zA-Z0-9]*=//'`
+      ;;
+  esac
 done
 
 ##############################################################
@@ -40,7 +46,7 @@ done
 # Check for the TAR file to make sure it exists
 if [ "${#}" == "0" ] || [ "${TARZ}" == "" ] || [ ! -e "${TARZ}" ]
 then
-  echo "Syntax error: make-openssl.sh {INPUT_TAR_GZ} {OUTPUT_FOLDER} (--clean) (--platform=mac_x86_64|ios_armv7|ios_armv7s|ios_simulator)"
+  echo "Syntax error: make-libpq.sh {INPUT_TAR_GZ} {OUTPUT_FOLDER} (--clean) (--platform=mac_x86_64|ios_armv7|ios_armv7s|ios_simulator)"
   exit 1
 fi
 
@@ -52,20 +58,20 @@ VERSION=`basename ${TARZ} | sed 's/\.tar\.gz//'`
 # Check for the BUILD directory
 if [ "${BUILD}XX" == "XX" ] || [ ! -d ${BUILD} ]
 then
-    echo "Syntax error: make-openssl.sh {INPUT_TAR_GZ} {OUTPUT_FOLDER}"
-    exit 1
+  echo "Syntax error: make-openssl.sh {INPUT_TAR_GZ} {OUTPUT_FOLDER}"
+  exit 1
 fi
 
 # Check for the UNARCHIVE  directories, use TMP if necessary
 if [ "${UNARCHIVE}" == "" ]
 then
-	UNARCHIVE=${TMPDIR}/${VERSION}/src
+    UNARCHIVE=${TMPDIR}/${VERSION}/src
 fi
 
 if [ ! -d ${UNARCHIVE} ]
 then
-	echo "mkdir ${UNARCHIVE}"
-	mkdir -pv ${UNARCHIVE}
+  echo "mkdir ${UNARCHIVE}"
+  mkdir -pv ${UNARCHIVE}
 fi
 
 ##############################################################
@@ -86,21 +92,21 @@ case ${PLATFORM} in
   mac_x86_64 )
     ARCH="x86_64"
     DEVROOT="${DEVELOPER_PATH}/Platforms/MacOSX.platform/Developer"
-	SDKROOT="${DEVROOT}/SDKs/MacOSX${MACOSX_DEPLOYMENT_TARGET}.sdk"
-	CC="/usr/bin/gcc -arch ${ARCH}"
-	CONFIGURE_FLAGS="darwin64-x86_64-cc no-shared no-krb5 no-zlib"
-	;;
+    SDKROOT="${DEVROOT}/SDKs/MacOSX${MACOSX_DEPLOYMENT_TARGET}.sdk"
+    CC="/usr/bin/gcc -arch ${ARCH}"
+    CONFIGURE_FLAGS="--enable-thread-safety --without-readline --with-bonjour --with-openssl --with-libxml --with-libxslt --disable-rpath"
+    ;;
   ios_armv7 )
     ARCH="armv7"
     DEVROOT="${DEVELOPER_PATH}/Platforms/iPhoneOS.platform/Developer"
     SDKROOT="${DEVROOT}/SDKs/iPhoneOS${IPHONE_DEPLOYMENT_TARGET}.sdk"
     CC="${DEVROOT}/usr/bin/gcc -arch ${ARCH}"
-    CONFIGURE_FLAGS="iphoneos-cross no-shared no-asm no-krb5 no-zlib"
+    CONFIGURE_FLAGS="--host=arm-apple-darwin --without-readline --disable-ipv6 --with-bonjour --with-openssl --with-libxml --with-libxslt --disable-rpath"
     ;;
   ios_armv7s )
     ARCH="armv7s"
     DEVROOT="${DEVELOPER_PATH}/Platforms/iPhoneOS.platform/Developer"
-	SDKROOT="${DEVROOT}/SDKs/iPhoneOS${IPHONE_DEPLOYMENT_TARGET}.sdk"
+    SDKROOT="${DEVROOT}/SDKs/iPhoneOS${IPHONE_DEPLOYMENT_TARGET}.sdk"
     CC="${DEVROOT}/usr/bin/gcc -arch ${ARCH}"
     CONFIGURE_FLAGS="iphoneos-cross no-shared no-asm no-krb5 no-zlib"
     ;;
@@ -110,13 +116,20 @@ case ${PLATFORM} in
     SDKROOT="${DEVROOT}/SDKs/iPhoneOS${IPHONE_DEPLOYMENT_TARGET}.sdk"
     CC="${DEVROOT}/usr/bin/gcc -arch ${ARCH}"
     CONFIGURE_FLAGS="iphoneos-cross no-shared no-asm no-krb5 no-zlib"
-	;;
+    ;;
   * )
     echo "Unknown build platform: ${PLATFORM}"
-	exit -1
+    exit 1
 esac
 
-CFLAGS="-isysroot ${SDKROOT}"
+CFLAGS="-isysroot ${SDKROOT} -arch ${ARCH}"
+LDFLAGS="-arch ${ARCH}"
+
+if [ -d "${OPENSSL}" ]
+then
+  CONFIGURE_FLAGS="${CONFIGURE_FLAGS} --with-includes=${OPENSSL}/${PLATFORM}/include --with-libs=${OPENSSL}/${PLATFORM}/lib"
+  echo "Using openssl libraries: ${OPENSSL}"
+fi
 
 ##############################################################
 # Check to see if already built, ignore if so
@@ -125,8 +138,8 @@ PREFIX=${BUILD}/${VERSION}/${PLATFORM}
 
 if [ -e ${PREFIX} ] && [ ${CLEAN} == 0 ]
 then
-  echo "Assuming already exists: ${PREFIX}"
-  exit 0
+echo "Assuming already exists: ${PREFIX}"
+exit 0
 fi
 
 ##############################################################
@@ -137,18 +150,12 @@ echo "    Build to: ${PREFIX}"
 echo "Architecture: ${ARCH}"
 echo "       Flags: ${CONFIGURE_FLAGS}"
 
-cd "${UNARCHIVE}/${VERSION}"
-./Configure ${CONFIGURE_FLAGS} --prefix=${PREFIX}
-make CC="${CC}" CFLAG="${CFLAGS}"
-make install_sw >/dev/null
+pushd "${UNARCHIVE}/${VERSION}"
+export CFLAGS="-arch x86_64"
+./configure CC="${CC}" CFLAGS="${CFLAGS}" LDFLAGS="${LDFLAGS}" --prefix="${PREFIX}" ${CONFIGURE_FLAGS}
+make -C src/interfaces/libpq
 
 
-##############################################################
-# Make symbolic links
 
-rm -f ${BUILD}/openssl-current-${PLATFORM}
-ln -s ${PREFIX} ${BUILD}/openssl-current-${PLATFORM}
-
-exit 0
 
 
