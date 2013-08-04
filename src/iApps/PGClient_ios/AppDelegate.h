40cb3d646eb910eca240275d5fda439dac6f6bd6
djthorpe <djt@mutablelogic.com>
2013-08-04 16:36:06 +0100

    Added

---
+++ src/iApps/PGClient_ios/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PGClient_ios
+//
+//  Created by David Thorpe on 04/08/2013.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ src/iApps/PGClient_ios/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PGClient_ios
+//
+//  Created by David Thorpe on 04/08/2013.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
