410b6ffc4a696e20d8a6f1db0ab9143ad6ef809b
djthorpe <djt@mutablelogic.com>
2013-08-04 16:59:13 +0100

    added PGClient_ios and dependencies

--- src/iApps/PGClient_ios/AppDelegate.h
+++ src/iApps/PGClient_ios/AppDelegate.h
@@ -7,6 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <PGClientKit_ios/PGClientKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
--- src/iApps/PGClient_ios/main.m
+++ src/iApps/PGClient_ios/main.m
@@ -1,13 +1,4 @@
-//
-//  main.m
-//  PGClient_ios
-//
-//  Created by David Thorpe on 04/08/2013.
-//
-//
-
 #import <UIKit/UIKit.h>
-
 #import "AppDelegate.h"
 
 int main(int argc, char *argv[])
