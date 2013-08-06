9a93305132f24da92711374aac2e348805eec190
David Thorpe <djt@mutablelogic.com>
2013-08-06 20:35:55 +0100

    updated

--- etc/build-all.sh
+++ etc/build-all.sh
@@ -9,10 +9,13 @@ CURRENT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 CONFIGURATION=Release
 PROJECT=${CURRENT_PATH}/../postgresql-kit.xcodeproj
 
-# Build iOS
+# Build iOS Framrworks
 xcodebuild -project ${PROJECT} -target "openssl_ios" -configuration ${CONFIGURATION} || exit -1
 xcodebuild -project ${PROJECT} -target "libpq_ios" -configuration ${CONFIGURATION} || exit -1
 xcodebuild -project ${PROJECT} -target "PGClientKit_ios" -configuration ${CONFIGURATION} || exit -1
+
+# Build iOS iApps
+# xcodebuild -project ${PROJECT} -target "PGClient_ios" -configuration ${CONFIGURATION} || exit -1
 
 # Build Mac Frameworks
 xcodebuild -project ${PROJECT} -target "openssl_mac" -configuration ${CONFIGURATION} || exit -1
