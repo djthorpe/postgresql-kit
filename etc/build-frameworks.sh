3cb663737f7ff44fae0f6e4e6e241e0c2f2f94c1
djthorpe <djt@mutablelogic.com>
2013-08-27 08:02:22 +0100

    Updated build script to include PGControlsKit

--- etc/build-frameworks.sh
+++ etc/build-frameworks.sh
@@ -13,16 +13,9 @@ xcodebuild -project ${PROJECT} -target "openssl_ios" -configuration ${CONFIGURAT
 xcodebuild -project ${PROJECT} -target "libpq_ios" -configuration ${CONFIGURATION} || exit -1
 xcodebuild -project ${PROJECT} -target "PGClientKit_ios" -configuration ${CONFIGURATION} || exit -1
 
-# Build iOS iApps
-# xcodebuild -project ${PROJECT} -target "PGClient_ios" -configuration ${CONFIGURATION} || exit -1
-
 # Build Mac Frameworks
 xcodebuild -project ${PROJECT} -target "openssl_mac" -configuration ${CONFIGURATION} || exit -1
 xcodebuild -project ${PROJECT} -target "postgresql_mac" -configuration ${CONFIGURATION} || exit -1
 xcodebuild -project ${PROJECT} -target "PGClientKit_mac" -configuration ${CONFIGURATION} || exit -1
 xcodebuild -project ${PROJECT} -target "PGServerKit_mac" -configuration ${CONFIGURATION} || exit -1
-
-# Build Mac Apps
-xcodebuild -project ${PROJECT} -target "PGServer" -configuration ${CONFIGURATION} || exit -1
-
-
+xcodebuild -project ${PROJECT} -target "PGControlsKit_mac" -configuration ${CONFIGURATION} || exit -1
