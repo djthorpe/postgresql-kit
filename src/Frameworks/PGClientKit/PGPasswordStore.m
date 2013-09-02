6b131752db0548165bc2068b792aa9407a6da754
David Thorpe <djt@mutablelogic.com>
2013-09-02 20:40:24 +0100

    added basic function definitions for PGPasswordStore

--- src/Frameworks/PGClientKit/PGPasswordStore.m
+++ src/Frameworks/PGClientKit/PGPasswordStore.m
@@ -3,4 +3,22 @@
 
 @implementation PGPasswordStore
 
+-(NSString* )passwordForURL:(NSURL* )url {
+	return [self passwordForURL:url error:nil];
+}
+
+-(BOOL)setPassword:(NSString* )password forURL:(NSURL* )url saveToKeychain:(BOOL)saveToKeychain {
+	return [self setPassword:password forURL:url saveToKeychain:saveToKeychain error:nil];
+}
+
+-(BOOL)setPassword:(NSString* )password forURL:(NSURL* )url saveToKeychain:(BOOL)saveToKeychain error:(NSError* )error {
+	// TODO
+	return nil;
+}
+
+-(NSString* )passwordForURL:(NSURL* )url error:(NSError** )error {
+	// TODO
+	return nil;
+}
+
 @end
