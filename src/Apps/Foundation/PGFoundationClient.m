5eddb9ce4be0e97d47203b8e97bf9708521c0d4b
David Thorpe <djt@mutablelogic.com>
2015-01-09 17:46:36 +0000

    added \roles command and renamed the other commands

--- src/Apps/Foundation/PGFoundationClient.m
+++ src/Apps/Foundation/PGFoundationClient.m
@@ -114,11 +114,14 @@
 // private methods
 
 -(NSString* )translate:(NSString* )statement {
-	if([statement isEqualToString:@"\\dd"]) {
+	if([statement isEqualToString:@"\\databases"]) {
 		return @"SELECT d.datname as \"Name\",pg_catalog.pg_get_userbyid(d.datdba) as \"Owner\",pg_catalog.pg_encoding_to_char(d.encoding) as \"Encoding\" FROM pg_catalog.pg_database d ORDER BY 1;";
 	}
-	if([statement isEqualToString:@"\\dt"]) {
+	if([statement isEqualToString:@"\\tables"]) {
 		return @"SELECT n.nspname as \"Schema\",c.relname as \"Name\",CASE c.relkind WHEN 'r' THEN 'table' WHEN 'v' THEN 'view' WHEN 'i' THEN 'index' WHEN 'S' THEN 'sequence' WHEN 's' THEN 'special' WHEN 'f' THEN 'foreign table' END as \"Type\",pg_catalog.pg_get_userbyid(c.relowner) as \"Owner\" FROM pg_catalog.pg_class c LEFT JOIN pg_catalog.pg_namespace n ON n.oid = c.relnamespace WHERE c.relkind IN ('r','') AND n.nspname <> 'pg_catalog' AND n.nspname <> 'information_schema' AND n.nspname !~ '^pg_toast' AND pg_catalog.pg_table_is_visible(c.oid) ORDER BY 1,2;";
+	}
+	if([statement isEqualToString:@"\\roles"]) {
+		return @"SELECT r.rolname as \"Name\", r.rolsuper AS \"Super\", r.rolinherit AS \"Inherit\",r.rolcreaterole, r.rolcreatedb, r.rolcanlogin,r.rolconnlimit, r.rolvaliduntil,ARRAY(SELECT b.rolname FROM pg_catalog.pg_auth_members m JOIN pg_catalog.pg_roles b ON (m.roleid = b.oid)  WHERE m.member = r.oid) as memberof , pg_catalog.shobj_description(r.oid, 'pg_authid') AS description, r.rolreplication FROM pg_catalog.pg_roles r";
 	}
 	if([statement isEqualToString:@"\\?"]) {
 		return @"SHOW ALL";
