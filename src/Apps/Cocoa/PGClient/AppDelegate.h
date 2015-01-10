db3fdeccb6b168f6b0614a0c4aa6493c8c0c9819
David Thorpe <djt@mutablelogic.com>
2015-01-10 16:33:14 +0000

    Added PGCocoaClient target

---
+++ src/Apps/Cocoa/PGClient/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PGCocoaClient
+//
+//  Created by David Thorpe on 10/01/2015.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ src/Apps/Cocoa/PGClient/AppDelegate.m
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.m
+//  PGCocoaClient
+//
+//  Created by David Thorpe on 10/01/2015.
+//
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+	// Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+	// Insert code here to tear down your application
+}
+
+@end
