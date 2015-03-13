a82749b84ee9716a1ac46ccaa264ecefcbd86930
David Thorpe <djt@mutablelogic.com>
2015-03-13 07:51:40 +0000

    changes to the test harness for PGControlsKit

--- src/Apps/Cocoa/ControlsTest/AppDelegate.m
+++ src/Apps/Cocoa/ControlsTest/AppDelegate.m
@@ -10,26 +10,81 @@
 #import <PGControlsKit/PGControlsKit.h>
 
 @interface AppDelegate ()
-
-@property (weak) IBOutlet NSWindow *window;
+@property (weak) IBOutlet NSWindow* window;
+@property (retain) PGConnection* connection;
+@property (retain) PGConnectionWindowController* connectionWindow;
+@property (retain) NSURL* url;
+@property (readonly) NSString* urlstring;
 @end
 
 @implementation AppDelegate
 
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-	// Insert code here to initialize your application
+@dynamic urlstring;
+
+-(NSString* )urlstring {
+	return [[self url] absoluteString];
 }
 
-- (void)applicationWillTerminate:(NSNotification *)aNotification {
-	// Insert code here to tear down your application
+-(void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+	// create connection class
+	[self setConnection:[PGConnection new]];
+	[self setConnectionWindow:[PGConnectionWindowController new]];
+	// get user default for url
+	NSString* url = [[NSUserDefaults standardUserDefaults] objectForKey:@"url"];
+	if(url) {
+		[super willChangeValueForKey:@"urlstring"];
+		[self setUrl:[NSURL URLWithString:url]];
+		[super didChangeValueForKey:@"urlstring"];
+	}
 }
 
+-(void)applicationWillTerminate:(NSNotification *)aNotification {
+	[[self connection] disconnect];
+}
+
+-(void)handleLoginError:(NSError* )error {
+	if([[error domain] isEqualToString:PGClientErrorDomain] && [error code]==PGClientErrorNeedsPassword) {
+		[[self connectionWindow] beginPasswordSheetWithParentWindow:[self window] whenDone:^(NSString* password, BOOL useKeychain) {
+			NSLog(@"SAVE PASSWORD: %@",error);
+		}];
+		return;
+	}
+
+	[[self connectionWindow] beginErrorSheetWithError:error parentWindow:[self window] whenDone:^(NSModalResponse response) {
+		if(response==NSModalResponseContinue) {
+			[self doCreateConnectionURL:nil];
+		}
+	}];
+
+}
 
 -(IBAction)doCreateConnectionURL:(id)sender {
-	PGConnectionWindowController* controller = [PGConnectionWindowController new];
-	[controller beginConnectionSheetWithURL:nil parentWindow:[self window] whenDone:^(NSURL *url) {
-		NSLog(@"GOT URL: %@",url);
+	[[self connectionWindow] beginConnectionSheetWithURL:[self url] parentWindow:[self window] whenDone:^(NSURL *url) {
+		if(url) {
+			// set the URL
+			[super willChangeValueForKey:@"urlstring"];
+			[self setUrl:url];
+			[super didChangeValueForKey:@"urlstring"];
+
+			// save URL
+			[[NSUserDefaults standardUserDefaults] setObject:[[self url] absoluteString] forKey:@"url"];
+			[[NSUserDefaults standardUserDefaults] synchronize];
+		}
 	}];
+}
+
+-(IBAction)doLogin:(id)sender {
+	if([self url]) {
+		[[self connection] connectWithURL:[self url] whenDone:^(BOOL usedPassword, NSError *error) {
+			if(error) {
+				[self handleLoginError:error];
+			}
+		}];
+	}
+}
+
+-(IBAction)doLogout:(id)sender {
+	[[self connection] disconnect];
 }
 
 @end
